4a2be06ff28149a6128d3939f44b345073b0fdb1
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2025-04-28 21:13:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let element = document.querySelectorAll('li');
+
+
+element.forEach((key,i)=>{
+    key.addEventListener('click',(e)=>{
+        alert(`The level of the element is: ${i+1}`)
+    })
+})
